41cb1b59eb663fbc09cdface6401db188a672325
LISA IUDICIANI <liudiciani@umass.edu>
2016-12-15 04:12:21 -0500

    fully dynamic now yayayay

--- mutants.js
+++ mutants.js
@@ -3,3 +3,15 @@ var killedMutants = [
     {num:"2",line:"2",mutation:"System.out.println('2 works');"},
     {num:"3",line:"3",mutation:"System.out.println('3 works');"}
   ]
+
+var liveMutants = [
+    {num:"4",line:"4",mutation:"System.out.println('4 works');"},
+    {num:"5",line:"5",mutation:"System.out.println('5 works');"},
+    {num:"6",line:"6",mutation:"System.out.println('6 works');"}
+  ]
+
+var uncoveredMutants = [
+    {num:"7",line:"7",mutation:"System.out.println('7 works');"},
+    {num:"8",line:"8",mutation:"System.out.println('8 works');"},
+    {num:"9",line:"9",mutation:"System.out.println('9 works');"}
+  ]
