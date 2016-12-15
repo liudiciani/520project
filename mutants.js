1923f835ecda608cb48eab928a89c108e1bc8f81
LISA IUDICIANI <liudiciani@umass.edu>
2016-12-15 16:24:38 -0500

    nothing changed cause it was all a waste of time :)

--- mutants.js
+++ mutants.js
@@ -1,17 +1,17 @@
 var killedMutants = [
-    {num:"1",line:"20",mutation:"System.out.println('1 works');"},
-    {num:"2",line:"2",mutation:"System.out.println('2 works');"},
-    {num:"3",line:"3",mutation:"System.out.println('3 works');"}
+    {num:"1",line:"10",mutation:"System.out.println('1 works');"},
+    {num:"2",line:"20",mutation:"System.out.println('2 works');"},
+    {num:"3",line:"30",mutation:"System.out.println('3 works');"}
   ]
 
 var liveMutants = [
-    {num:"4",line:"4",mutation:"System.out.println('4 works');"},
-    {num:"5",line:"5",mutation:"System.out.println('5 works');"},
-    {num:"6",line:"6",mutation:"System.out.println('6 works');"}
+    {num:"4",line:"13",mutation:"System.out.println('4 works');"},
+    {num:"5",line:"53",mutation:"System.out.println('5 works');"},
+    {num:"6",line:"24",mutation:"System.out.println('6 works');"}
   ]
 
 var uncoveredMutants = [
-    {num:"7",line:"7",mutation:"System.out.println('7 works');"},
-    {num:"8",line:"8",mutation:"System.out.println('8 works');"},
-    {num:"9",line:"9",mutation:"System.out.println('9 works');"}
+    {num:"7",line:"17",mutation:"System.out.println('7 works');"},
+    {num:"8",line:"28",mutation:"System.out.println('8 works');"},
+    {num:"9",line:"39",mutation:"System.out.println('9 works');"}
   ]
